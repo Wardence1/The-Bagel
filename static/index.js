f387ea7230e35d6736ab36bcedb8117e3ad17ef4
GingerTron <130793828+GingerTron1234@users.noreply.github.com>
2023-06-28 20:04:39 -0400

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -1,1 +1,11 @@
 console.log("why are you in the log?");
+
+let username;
+document.getElementById("name").innerHTML = "Hello,";
+
+document.getElementById("nameButton").onclick = function() {
+    
+    username = document.getElementById("nameText").value;
+    document.getElementById("name").innerHTML = "Hello, " + username;
+}
+    
