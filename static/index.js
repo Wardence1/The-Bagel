a7bd23198ebd0fa20a19264bfb62b351a8583b45
GingerTron <130793828+GingerTron1234@users.noreply.github.com>
2023-06-30 16:57:48 -0400

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -6,6 +6,15 @@ document.getElementById("name").innerHTML = "Hello,";
 document.getElementById("nameButton").onclick = function() {
     
     username = document.getElementById("nameText").value;
+    if (username == 69) 
+    {username = "(;"} 
+    else if (username == "I like bagels")
+    {username = "I Like Bagels Too"}
+    else if (username == "Jeb_")
+    {username = "Add toilets to Minecraft"}
+    else if (username == "Jackson Kidwell")
+    {username = "Me"}
+
     document.getElementById("name").innerHTML = "Hello, " + username;
 }
     
