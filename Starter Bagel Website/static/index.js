69061816a13eca7264f0411bd7e0fbfa5881f935
Jackson Kidwell <130793828+GingerTron1234@users.noreply.github.com>
2023-08-25 18:36:52 -0400

    Add files via upload

---
+++ Starter Bagel Website/static/index.js
@@ -0,0 +1,20 @@
+console.log("why are you in the log?");
+
+let username;
+document.getElementById("name").innerHTML = "Hello,";
+
+document.getElementById("nameButton").onclick = function() {
+    
+    username = document.getElementById("nameText").value;
+    if (username == 69) 
+    {username = "(;"} 
+    else if (username == "I like bagels")
+    {username = "I Like Bagels Too"}
+    else if (username == "Jeb_")
+    {username = "add toilets to Minecraft!!!"}
+    else if (username == "Jackson Kidwell" || username == "Jackson")
+    {username = "Me"}
+
+    document.getElementById("name").innerHTML = "Hello, " + username;
+}
+    
